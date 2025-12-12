b994891c26f541dead350ae08d13dc6aaa52c7cc
MARON <rudenko@rud-m.ru>
2025-12-12 06:27:16 +0500

    add data

--- TableDataViewer/Program.cs
+++ TableDataViewer/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿namespace TableDataViewer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string folder, file, pagination, columns;
+			
+			if (args.Length != 4)
+			{
+				Console.WriteLine("Введите путь до файла: ");
+				folder = Console.ReadLine();
+			
+				Console.WriteLine("Введите имя файла: ");
+				file = Console.ReadLine();
+			
+				Console.WriteLine("Введите пагинацию: ");
+				pagination = Console.ReadLine();
+			
+				Console.WriteLine("Введите столбцы: ");
+				columns = Console.ReadLine();
+			}
+			else
+			{
+				folder = args[0];
+				file = args[1];
+				pagination = args[2];
+				columns = args[3];
+			}
+		}
+	}
+}
